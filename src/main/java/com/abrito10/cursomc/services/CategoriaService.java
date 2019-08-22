476d24ed2fdee3eec804d44282cda1ee8b1c1d80
Adilson Brito <abrito10@gmail.com>
2019-08-22 20:14:54 -0300

    Criando DAO e Services para Categoria

---
+++ src/main/java/com/abrito10/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.abrito10.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.abrito10.cursomc.dao.CategoriaDAO;
+import com.abrito10.cursomc.domain.Categoria;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaDAO repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id); 
+		return obj.orElse(null);
+	}
+}
