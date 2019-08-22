532414dfcdde6206ca01f5c07c61345cabfe06e5
Adilson Brito <abrito10@gmail.com>
2019-08-22 20:27:13 -0300

    Criando operacao de instanciacao

--- src/main/java/com/abrito10/cursomc/CursomcApplication.java
+++ src/main/java/com/abrito10/cursomc/CursomcApplication.java
@@ -1,13 +1,30 @@
 package com.abrito10.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.abrito10.cursomc.dao.CategoriaDAO;
+import com.abrito10.cursomc.domain.Categoria;
+import com.sun.xml.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
+@SpringBootApplication
+public class CursomcApplication  implements CommandLineRunner{
+	@Autowired
+	private CategoriaDAO categoriaDao;
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaDao.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
