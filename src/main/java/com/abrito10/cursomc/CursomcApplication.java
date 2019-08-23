8d8c58787419c15b92dae5402b42d825459244d3
Adilson Brito <abrito10@gmail.com>
2019-08-22 21:28:53 -0300

    Produto e associacao muitos pra muitos

--- src/main/java/com/abrito10/cursomc/CursomcApplication.java
+++ src/main/java/com/abrito10/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.abrito10.cursomc;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +9,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.abrito10.cursomc.dao.CategoriaDAO;
+import com.abrito10.cursomc.dao.ProdutoDAO;
 import com.abrito10.cursomc.domain.Categoria;
+import com.abrito10.cursomc.domain.Produto;
 import com.sun.xml.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
 @SpringBootApplication
 public class CursomcApplication  implements CommandLineRunner{
+	
 	@Autowired
 	private CategoriaDAO categoriaDao;
+	
+	@Autowired
+	private ProdutoDAO produtoDao;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -24,7 +32,22 @@ public class CursomcApplication  implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null,"Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null,"Computador",2000.00);
+		Produto p2 = new Produto(null,"Impressora",800.00);
+		Produto p3 = new Produto(null,"Mouse",80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
+
+		
 		categoriaDao.saveAll(Arrays.asList(cat1,cat2));
+		produtoDao.saveAll(Arrays.asList(p1,p2,p3));
+		
 	}
 
 }
