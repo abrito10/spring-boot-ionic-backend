701737d1d1aa0d1c97fd3ca487ba74032c34f1ce
Adilson Brito <abrito10@gmail.com>
2019-08-31 21:05:30 -0300

    Personalizando o retorno da validação

---
+++ src/main/java/com/abrito10/cursomc/resources/exception/FieldMessage.java
@@ -0,0 +1,37 @@
+package com.abrito10.cursomc.resources.exception;
+
+import java.io.Serializable;
+
+public class FieldMessage implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String fieldame;
+	private String message;
+	
+	public FieldMessage() {
+	}
+
+	public FieldMessage(String fieldame, String message) {
+		super();
+		this.fieldame = fieldame;
+		this.message = message;
+	}
+
+	public String getFieldame() {
+		return fieldame;
+	}
+
+	public void setFieldame(String fieldame) {
+		this.fieldame = fieldame;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
