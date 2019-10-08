5344266e92c9203b9fb89cb135add6d535dc2578
Adilson Brito <abrito10@gmail.com>
2019-10-08 20:56:07 -0300

    86. Usando padrão de nomes para imagens

--- src/main/java/com/abrito10/cursomc/domain/Cliente.java
+++ src/main/java/com/abrito10/cursomc/domain/Cliente.java
@@ -58,9 +58,7 @@ public class Cliente implements Serializable {
 	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
-	
-	private String imageUrl;
-	
+		
 	public Cliente() {	
 		addPerfil(Perfil.CLIENTE);
 	}
@@ -181,14 +179,4 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
-
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
-	
-	
 }
